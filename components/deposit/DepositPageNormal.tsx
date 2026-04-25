604d828c774b8a6233797bbe9d3109c5aa562f85
saksun00751 <saksun00751@gmail.com>
2026-04-25 21:07:39 +0700

    edit payment 2

--- components/deposit/DepositPageNormal.tsx
+++ components/deposit/DepositPageNormal.tsx
@@ -387,7 +387,7 @@ function BankCard({
   onClick: () => void;
   onCopy: (accountNo: string) => void;
 }) {
-  const minAmt = Math.max(parseFloat(account.deposit_min) || 0, 100);
+  const minAmt = parseFloat(account.deposit_min) || 0;
   return (
     <div
       role="button"
